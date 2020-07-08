7a9c2f01f74b8a47e2f338653f8ba6399d4a79a1
wischli <w.freude@icloud.com>
2020-07-08 11:43:46 +0200

    add rust workshop #1

---
+++ workshop/difference-of-squares/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    unimplemented!("Calculate the square of a sum (a_1 + ... + a_n)^2")
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    unimplemented!("Calculate the sum of squares (a_1^2 + ... + a_n^2)")
+}
+
+pub fn difference(n: u32) -> u32 {
+    unimplemented!("Calculate the difference");
+    square_of_sum(n) - sum_of_squares(n)
+}
